943d5bc03fd3010edaa15db4cb8d410cb184383d
wasim6038 <160705270+wasim6038@users.noreply.github.com>
2024-08-18 20:15:29 +0000

    datatypes

---
+++ 01/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // It is not working because we use nodejs not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Wasim")
+
+
+let name = "wasim"
+let age = 25
+let isLoggedIn = true
+let state;
+
+// Some Datatypes are
+
+// number => 2 to power 53 (range)
+// bigint => use for bigger number out of the number range
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
