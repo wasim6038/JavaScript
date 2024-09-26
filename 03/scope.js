7a56519ebebc9c566f2fea76ce105b11f7ba221f
wasim6038 <160705270+wasim6038@users.noreply.github.com>
2024-09-26 17:29:49 +0000

    scope

--- 03/scope.js
+++ 03/scope.js
@@ -6,9 +6,35 @@ if (true) {   /**Block Scope */
     let a = 28
     const b = 20
     var c = 10
-    console.log("INNER:", a);
+    // console.log("INNER:", a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(c);
+
+
+function one(){
+    const username = "Wasim"
+
+    function two(){
+        const password = "@123"
+        console.log(username);
+        
+    }
+    // console.log(password);    /**error because of scope */
+    two()
+}
+// one()
+
+if (true) {
+    const username = "wasim"
+    if (username === "wasim") {
+        const password = " ansari"
+        console.log(username + password);
+    }
+    // console.log(password);   /**error because of scope */
+    
+}
+// console.log(username);   /**error because of scope */
+
