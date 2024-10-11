f248f4ec9ae789f5df58574878f6be88ef5911bd
wasim6038 <160705270+wasim6038@users.noreply.github.com>
2024-10-11 19:45:54 +0000

    while loop

---
+++ 05_loops/two.js
@@ -0,0 +1,28 @@
+// While and do-while loops
+
+
+// while loop
+
+let index = 1
+while (index <= 10) {
+    // console.log(`Value is ${index}`);
+    // index = index + 2
+    index = index + 1
+}
+
+const myArray = ["Wasim", "Ansari", "Akram", "Anas", "Saifi"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(myArray[arr]);
+    arr++
+}
+
+// do-while loop
+
+let num = 19
+do {
+    console.log(num);
+    num++
+    
+} while (num < 9);
